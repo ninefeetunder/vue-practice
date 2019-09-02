81f83cfee86244f0be9a945d349aa2be86bf5a29
ninefeetunder <1285499453@qq.com>
2019-09-02 19:48:31 +0800

    city页添加搜索框

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    line-height: .72rem
+    .search-input
+      height: .52rem
+      width: 100%
+      padding: 0 .1rem
+      box-sizing: border-box
+      border-radius: .06rem
+      text-align: center
+      color: #ccc
+</style>
