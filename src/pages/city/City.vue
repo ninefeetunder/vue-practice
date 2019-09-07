88d9130158ed3e401e5b5189a67b995ec679a971
ninefeetunder <1285499453@qq.com>
2019-09-07 17:22:02 +0800

    添加city组件的名字

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -14,6 +14,7 @@ import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
 import axios from 'axios'
 export default {
+  name: 'City',
   components: {
     CityHeader,
     CitySearch,
