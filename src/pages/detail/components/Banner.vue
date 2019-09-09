3c77feb7315c5803a667d760357fc77b43955b04
ninefeetunder <1285499453@qq.com>
2019-09-09 19:00:56 +0800

    完成Detail组件header渐隐渐现效果

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="wrapper">
-    <div class="iconfont back-icon" @click="goBack">&#xe667;</div>
     <div class="banner" @click="handleBannerClick">
       <img
         class="banner-img"
@@ -32,10 +31,6 @@ export default {
     }
   },
   methods: {
-    goBack () {
-      this.showGallery = false
-      this.$router.go(-1)
-    },
     handleGalleryClose () {
       this.showGallery = false
     },
@@ -50,48 +45,33 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .wrapper
+  .banner
+    height: 0
+    padding-bottom: 55%
+    overflow: hidden
     position: relative
-    .back-icon
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
       position: absolute
-      top: .1rem
-      left: .1rem
-      width: .72rem
-      height: .72rem
-      line-height: .72rem
-      border-radius: 50%
-      background: rgba(0,0,0,.7)
+      left: 0
+      right: 0
+      bottom: 0
+      height: .6rem
       color: #fff
-      text-align: center
-      font-size: .4rem
-      z-index: 2
-    .banner
-      height: 0
-      padding-bottom: 55%
-      overflow: hidden
-      position: relative
-      .banner-img
-        width: 100%
-      .banner-info
-        display: flex
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: .6rem
-        color: #fff
-        background: linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,.8))
-        .banner-title
-          height: .4rem
-          font-size: .36rem
-          flex: 1
-        .banner-number
-          padding: 0 .3rem
-          height: .4rem
-          line-height: .4rem
-          font-size: .24rem
-          background: rgba(0,0,0,.5)
-          border-radius: .2rem
-          .banner-icon
-            font-size: .3rem
+      background: linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,.8))
+      .banner-title
+        height: .4rem
+        font-size: .36rem
+        flex: 1
+      .banner-number
+        padding: 0 .3rem
+        height: .4rem
+        line-height: .4rem
+        font-size: .24rem
+        background: rgba(0,0,0,.5)
+        border-radius: .2rem
+        .banner-icon
+          font-size: .3rem
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="header-abs" v-show="showAbs">
+      <div class="iconfont back-icon" @click="goBack">&#xe667;</div>
+    </div>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <div class="title">
+        景点详情
+        <div class="iconfont header-back" @click="goBack">&#xe667;</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    goBack () {
+      this.$router.go(-1)
+    },
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 42) {
+        this.showAbs = false
+        let opacity = top / 250
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {
+          opacity
+        }
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+    document.documentElement.scrollTo(0, 0)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .back-icon
+    position: absolute
+    top: .1rem
+    left: .1rem
+    width: .72rem
+    height: .72rem
+    line-height: .72rem
+    border-radius: 50%
+    background: rgba(0,0,0,.7)
+    color: #fff
+    text-align: center
+    font-size: .4rem
+  .header-fixed
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    .title
+      position: relative
+      height: $headerHeight
+      line-height: $headerHeight
+      color: #fff
+      text-align: center
+      font-size: .32rem
+      background: $bgColor
+      .header-back
+        width: .64rem
+        height: $headerHeight
+        line-height: $headerHeight
+        text-align: center
+        font-size: .4rem
+        position: absolute
+        left: 0
+        top: 0
+</style>
